4ce1d0f8451aa9645d46dbccbefab91f70b90168
kushal <kushalthapa1373@gmail.com>
2025-01-17 17:04:00 +0545

    Sprint third changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,22 @@
-import 'package:flutter/material.dart';
-import 'package:futsal_booking/app.dart';
 
-void main() {
-  runApp(const MyApp());
+import 'package:flutter/material.dart';
+import 'package:futsal_booking/app/di/di.dart';
+import 'package:futsal_booking/app/widget/app.dart';
+import 'package:futsal_booking/core/network/hive_service.dart';
+
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  // Initialize Hive Database
+  await HiveService.init();
+
+  // Initialize Dependencies
+
+  // await HiveService().clearStudentBox();
+
+  await initDependencies();
+
+  runApp(
+    App(),
+  );
 }
