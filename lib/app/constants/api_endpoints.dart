8bca3c47e7ac9003e73022e70cd41a4a0e5e4f8f
kushal <kushalthapa1373@gmail.com>
2025-02-21 20:33:13 +0545

    Updated API endpoints for booking

--- lib/app/constants/api_endpoints.dart
+++ lib/app/constants/api_endpoints.dart
@@ -2,7 +2,7 @@ class ApiEndpoints {
   ApiEndpoints._();
   static const Duration connectionTimeout = Duration(seconds: 1000);
   static const Duration receiveTimeout = Duration(seconds: 1000);
-  static const String baseUrl = "http://10.0.2.2:3000/api/v1/";
+  static const String baseUrl = "http://10.0.2.2:3000/api/";
 
   static const String login = "auth/login";
   static const String register = "auth/register";
